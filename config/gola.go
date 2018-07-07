b2188f55fbfadc603f9224955560c9bce064e674
jefurry <jefurry@qq.com>
2018-07-08 07:10:30 +0800

    add gola config

---
+++ config/gola.go
@@ -0,0 +1,15 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Gola
+//
+// Copyright 2018 The Gola Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package config
+
+const (
+	PROJECT_NAME = "Gola"
+	VERSION      = "0.0.1"
+	AUTHOR       = "Jeff <jefurry@qq.com>"
+)
