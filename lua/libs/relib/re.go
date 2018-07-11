dc79ba76a7014ed47415f9b24dd10494b15d6db8
jefurry <jefurry@qq.com>
2018-07-11 14:19:56 +0800

    implements relib for Lua

---
+++ lua/libs/relib/re.go
@@ -0,0 +1,23 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Gola
+//
+// Copyright 2018 The Gola Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package relib implements re for Lua.
+package relib
+
+import (
+	"github.com/yuin/gluare"
+	"github.com/yuin/gopher-lua"
+)
+
+const (
+	ReLibName = "re"
+)
+
+func Open(L *lua.LState) {
+	L.PreloadModule(ReLibName, gluare.Loader)
+}
