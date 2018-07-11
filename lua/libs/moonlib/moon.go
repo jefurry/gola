0e563a5374cf32c7d6682755217da4a7e8c777d8
jefurry <jefurry@qq.com>
2018-07-11 14:19:19 +0800

    implements moonlib for Lua

---
+++ lua/libs/moonlib/moon.go
@@ -0,0 +1,23 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Gola
+//
+// Copyright 2018 The Gola Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package moonlib implements moon for Lua.
+package moonlib
+
+import (
+	"github.com/rucuriousyet/gmoonscript"
+	"github.com/yuin/gopher-lua"
+)
+
+const (
+	MoonLibName = "moon"
+)
+
+func Open(L *lua.LState) {
+	L.PreloadModule(MoonLibName, gmoonscript.Loader)
+}
